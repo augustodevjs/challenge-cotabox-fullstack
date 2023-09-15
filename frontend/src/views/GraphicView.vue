bd9184c993593da3551be22d5609d2516a3fbb51
augusto <augustosaaude@gmail.com>
2023-09-15 18:58:10 -0300

    feat: add design of project

--- frontend/src/views/GraphicView.vue
+++ frontend/src/views/GraphicView.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
+import ChartBar from '../shared/components/chart-bar/ChartBar.vue';
 </script>
 
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <ChartBar />
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
 }
 </style>
