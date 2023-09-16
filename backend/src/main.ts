684323046e052020e920b89d19948df6c6e5960b
João Augusto <augustosaaude@gmail.com>
2023-09-16 18:20:45 -0300

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const configService = app.get(ConfigService);
   const port = configService.get('port');
