c83b2e0924d6c975a017e168068a1f52555a80f4
João Augusto <augustosaaude@gmail.com>
2023-09-18 19:48:42 -0300

    feat: add some changes

--- backend/src/shared/config/typeorm.config.ts
+++ backend/src/shared/config/typeorm.config.ts
@@ -6,13 +6,12 @@ export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      "host": process.env.DB_HOST,
-      "port": +process.env.DB_PORT,
-      "username": process.env.DB_USERNAME,
-      "password": process.env.DB_PASSWORD,
-      "database": process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/../entities/*.entity{.ts,.js}'],
-      synchronize: true,
     };
   }
 }
